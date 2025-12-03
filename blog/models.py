ecf803881ca17d1f0e273408231aaf4354f18922
alberto.nessi <albertonessi1@gmail.com>
2025-12-03 22:12:13 +0100

    new Books and Quotes models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Quotes(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=100)
+    date = models.DateTimeField('publishing date')
+    text = models.TextField()
+    link = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=100)
+    link = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
